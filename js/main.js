831ee259cf3fec7db8c738863884c25da8687311
SEC-MASUM <masum.jess42@gmail.com>
2022-03-10 19:20:01 +0600

    script src error fixed, fetchIssues() error fixed

--- js/main.js
+++ js/main.js
@@ -1,49 +1,59 @@
-document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+document
+  .getElementById("issueInputForm")
+  .addEventListener("submit", submitIssue);
 
 function submitIssue(e) {
-  const getInputValue = id => document.getElementById(id).value;
-  const description = getInputValue('issueDescription');
-  const severity = getInputValue('issueSeverity');
-  const assignedTo = getInputValue('issueAssignedTo');
-  const id = Math.floor(Math.random()*100000000) + '';
-  const status = 'Open';
+  const getInputValue = (id) => document.getElementById(id).value;
+  const description = getInputValue("issueDescription");
+  const severity = getInputValue("issueSeverity");
+  const assignedTo = getInputValue("issueAssignedTo");
+  const id = Math.floor(Math.random() * 100000000) + "";
+  const status = "Open";
 
   const issue = { id, description, severity, assignedTo, status };
   let issues = [];
-  if (localStorage.getItem('issues')){
-    issues = JSON.parse(localStorage.getItem('issues'));
+  if (localStorage.getItem("issues")) {
+    issues = JSON.parse(localStorage.getItem("issues"));
   }
   issues.push(issue);
-  localStorage.setItem('issues', JSON.stringify(issues));
+  localStorage.setItem("issues", JSON.stringify(issues));
 
-  document.getElementById('issueInputForm').reset();
+  document.getElementById("issueInputForm").reset();
   fetchIssues();
   e.preventDefault();
 }
 
-const closeIssue = id => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
-  currentIssue.status = 'Closed';
-  localStorage.setItem('issues', JSON.stringify(issues));
+const closeIssue = (id) => {
+  const issues = JSON.parse(localStorage.getItem("issues"));
+  const currentIssue = issues.find((issue) => issue.id === id);
+  currentIssue.status = "Closed";
+  localStorage.setItem("issues", JSON.stringify(issues));
   fetchIssues();
-}
+};
 
-const deleteIssue = id => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
-  localStorage.setItem('issues', JSON.stringify(remainingIssues));
-}
+const deleteIssue = (id) => {
+  const issues = JSON.parse(localStorage.getItem("issues"));
+  const remainingIssues = issues.filter(issue.id !== id);
+  localStorage.setItem("issues", JSON.stringify(remainingIssues));
+};
 
 const fetchIssues = () => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const issuesList = document.getElementById('issuesList');
-  issuesList.innerHTML = '';
+  const issues = localStorage.getItem("issues");
+  let issuesObj;
+  if (issues) {
+    issuesObj = JSON.parse(issues);
+  } else {
+    issuesObj = {};
+    return;
+  }
 
-  for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
+  const issuesList = document.getElementById("issuesList");
+  issuesList.innerHTML = "";
 
-    issuesList.innerHTML +=   `<div class="well">
+  for (var i = 0; i < issuesObj.length; i++) {
+    const { id, description, severity, assignedTo, status } = issues[i];
+
+    issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
@@ -53,4 +63,4 @@ const fetchIssues = () => {
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
-}
+};
